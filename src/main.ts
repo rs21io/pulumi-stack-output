eb6122bd21fc0a7d4549f0cc5fa0cac626971982
James Arnold <69863593+jmsarn@users.noreply.github.com>
2023-05-09 07:55:18 -0600

    Fix test-build workflow-parameters (#5)
    * Fix test-build workflow-parameters
    
    * Fix compile errors
    
    * Test secret value
    
    * Remove secrets test
    
    * Add token permissions
    
    * Add debug statement
    
    * Change message type
    
    * Update dist
    
    * Fix key construction
    
    * Rebuild dist
    
    * Add debug to JSON document
    
    * Use transformToString to read file body
    
    * Update tests
    
    * Remove debug

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import * as core from '@actions/core'
-import { S3Client, GetObjectCommand } from '@aws-sdk/client-s3'
-import { resourceUsage } from 'process'
+import {S3Client, GetObjectCommand} from '@aws-sdk/client-s3'
 
 export async function run(): Promise<void> {
   try {
@@ -10,22 +9,23 @@ export async function run(): Promise<void> {
 
     if (!s3Uri.endsWith('/')) s3Uri += '/'
     const bucket = s3Uri.split('/')[2]
-    const prefix = s3Uri.split('/').slice(3, -1).join('/')
-    const key = `${prefix}/.pulumi/stacks/${stackName}.json`
-    console.log(bucket);
+    const prefix = s3Uri.split('/').slice(3).join('/')
+    const key = `${prefix}.pulumi/stacks/${stackName}.json`
 
-    const s3 = new S3Client({});
-    const command = new GetObjectCommand({ Bucket: bucket, Key: key });
-    const response = await s3.send(command);
-    const body = response.Body?.toString();
+    const s3 = new S3Client({})
+    const command = new GetObjectCommand({Bucket: bucket, Key: key})
+    const response = await s3.send(command)
+    const body = await response.Body?.transformToString()
     if (!body) {
-      throw new Error(`Failed to read ${stackName} from ${s3Uri}`);
+      throw new Error(`Failed to read ${stackName} from ${s3Uri}`)
     }
 
-    const state = JSON.parse(body);
+    const state = JSON.parse(body)
     const latestKey = 'latest' in state.checkpoint ? 'latest' : 'Latest'
     const resources = state.checkpoint[latestKey].resources
-    const stack = resources.find((r: any) => r.type === 'pulumi:pulumi:Stack')
+    const stack = resources.find(
+      (r: Record<string, string>) => r.type === 'pulumi:pulumi:Stack'
+    )
     const output = stack.outputs[outputName]
     core.setOutput(outputName, output)
   } catch (error) {
